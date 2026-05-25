4d34c25425c10b13052f928abb46d82d9805f3f0
Fiona <b10705037@ntu.edu.tw>
2026-05-25 23:06:27 +0800

    frontend: import pages, package updated

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,14 @@
 <template>
-  <RouterView />
+  <!-- <TravelDashboard /> -->
+  <!-- <ItineraryPage /> -->
+  <!-- <FlightPage />   -->
+  <!-- <ItemPage /> -->
 </template>
+
+<script setup>
+// 把你放在同一層的 TravelDashboard 引進來
+import TravelDashboard from './TravelDashboard.vue';
+import ItineraryPage from './pages/ItineraryPage.vue';
+import FlightPage from './pages/FlightPage.vue';
+import ItemPage from './pages/ItemPage.vue';  
+</script>
